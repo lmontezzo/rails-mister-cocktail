a2d1b0950223b7549f7e7b95ea858c9767bc17e4
Luciano Montezzo <lmontezzo@gmail.com>
2017-04-13 15:40:57 -0300

    show updated

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,9 +13,38 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to @cocktail, notice: 'Cocktail was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path, notice: 'Cocktail was successfully destroyed.'
+  end
+
+
   private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
